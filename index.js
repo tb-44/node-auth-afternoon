2660f8182e752a2e23af05dab48ba7dcb6d7c787
Trent Bennett <rehab.rtb@gmail.com>
2017-09-06 15:21:28 -0600

    modified

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const request = require('request');
+const strategy = require('./strategy.js');
 
 const app = express();
+
 app.use( session({
-  secret: '@nyth!ng y0u w@nT',
+  secret: 'how you doing?',
   resave: false,
   saveUninitialized: false
 }));
+
+app.use( passport.initialize() );
+app.use( passport.session() );
+passport.use( strategy );
+
+passport.serializeUser( (user, done) => {
+  const { _json } = user;
+  done(null, {
+    clientID: _json.clientID,
+    email: _json.email,
+    name: _json.name,
+    followers: _json.followers_url });
+});
+
+passport.deserializeUser( (obj, done) => {
+  done(null, obj);
+});
+
+app.get( '/login',
+  passport.authenticate('auth0', {
+    successRedirect: '/followers',
+    failureRedirect: '/login',
+    failureFlash: true,
+    connection: 'github' }
+  )
+);
+
+app.get('/followers', ( req, res, next ) => {
+  if ( req.user ) {
+    const followersRequest = {
+      url: req.user.followers,
+      headers: {
+        'User-Agent': req.user.clientID
+      }
+    };
+
+    request(followersRequest, ( error, response, body ) => {
+      res.status(200).send(body);
+    });
+  } else {
+    res.redirect('/login');
+  }
+});
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
